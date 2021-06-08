15673c8df79986cb38f457910156972302f1f890
Andrew Ton <at965@grace1.grace.hpc.yale.internal>
2021-06-08 11:06:52 -0400

    using more modern version of matlab on slurm

--- bash/fracture/drawJamFracSimsSubmit.sh
+++ bash/fracture/drawJamFracSimsSubmit.sh
@@ -2,10 +2,13 @@
 #SBATCH -J matlabDraw
 #SBATCH --output=out/matlabDraw.out
 #SBATCH --error=out/matlabDraw.err
+#SBATCH --chdir=/home/at965/cells
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=andrewtondata@gmail.com
 #SBATCH -c 4
 #SBATCH -t 6:00:00
 #SBATCH -p pi_ohern,day,scavenge
 
-module load MATLAB/2016b
+module load MATLAB/2019b
 #matlab -nodisplay -nosplash -r YourFunction < /dev/null
-matlab -nodisplay -nosplash -r < drawJamFracSims.m
+matlab -nodisplay -nosplash -r drawJamFracSims < /dev/null
